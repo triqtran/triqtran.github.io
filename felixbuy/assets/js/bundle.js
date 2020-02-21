3f837ca0c5686653bfe2a8dab7b4ce2cd30b5e9e
Bruce Tran <tri.mpcgroup@gmail.com>
2020-02-21 14:25:02 +0700

    Update profile ppage

--- felixbuy/assets/js/bundle.js
+++ felixbuy/assets/js/bundle.js
@@ -1,3 +1,22 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      const imgID = $(input).data('img');
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#' + imgID).attr('src', e.target.result);
+      };
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
 $(document).ready(function () {
-  
+  $('.custom-select').each(function (index, item) {
+    var text = $(item).data('placeholder');
+    $(item).prepend('<option selected>' + text + '</option>');
+  });
+  $('.datepicker').datepicker({
+    format: "dd/mm/yyyy",
+    autoclose: true
+  });
 })
